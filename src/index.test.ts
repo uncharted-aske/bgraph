fc6baea9dc84fca5ca43b087cfd129617f274916
Adamo Carolli <adamo.carolli@gmail.com>
2021-06-28 18:14:07 -0400

    [LINT] Ignore faulty lint errors

--- src/index.test.ts
+++ src/index.test.ts
@@ -31,6 +31,8 @@ describe("tree triangle graph", () => {
   });
   // SIMPLE TRAVERSAL
   test("retrieve all children", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v('A').out().run();
     result = result.map(d => d.vertex._id);
 
@@ -49,6 +51,8 @@ describe("cyclic triangle graph", () => {
   });
   // SIMPLE TRAVERSAL
   test("complete cycle", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v('A').out().out().out().run();
     result = result.map(d => d.vertex._id);
 
@@ -56,6 +60,8 @@ describe("cyclic triangle graph", () => {
   });
   // START/REPEAT
   test("repeat around cycle", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v('A').start().out().repeat(2).run();
     result = result.map(d => d.vertex._id);
 
@@ -63,6 +69,8 @@ describe("cyclic triangle graph", () => {
   });
   // SUSPEND/UNSUSPEND
   test("suspend each visited node in cycle", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v('A').start().suspend().out().repeat(2).filter(d => false).run();
     result = result.map(d => d.vertex._id);
 
@@ -70,6 +78,8 @@ describe("cyclic triangle graph", () => {
   });
   // TAKE
   test("take a single output", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v('A').start().suspend().out().repeat(2).filter(d => false).take(1).run();
     result = result.map(d => d.vertex._id);
 
@@ -77,12 +87,16 @@ describe("cyclic triangle graph", () => {
   });
   // AS (ALIAS)/BACK
   test("alias the start node and then move back", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v('A').as('start').out().back('start').run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual(['A']);
   });
   test("do not move back to a filtered alias", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v('A').as('start').out().filter(d => false).back('start').run();
     result = result.map(d => d.vertex._id);
 
@@ -106,18 +120,24 @@ describe("cyclic single node graph", () => {
     expect(result).toEqual(['n-1']);
   });
   test("vertex: label filter", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v('B').run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual([]);
   });
   test("vertex: object filter passes", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v({ color: 'green' }).run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual(['n-1']);
   });
   test("vertex: object filter fails", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v({ color: 'red' }).run();
     result = result.map(d => d.vertex._id);
 
@@ -125,24 +145,32 @@ describe("cyclic single node graph", () => {
   });
   // FILTER PIPE
   test("filter: object filter passes", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
   let result = G.v().filter({ color: 'green' }).run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual(['n-1']);
   });
   test("filter: object filter fails", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().filter({ color: 'red' }).run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual([]);
   });
   test("filter: function filter fails", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().filter(d => d.score > 0.90 ).run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual([]);
   });
   test("filter: function filter passes", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().filter(d => d.score > 0.30 ).run();
     result = result.map(d => d.vertex._id);
 
@@ -150,30 +178,40 @@ describe("cyclic single node graph", () => {
   });
   // SIMPLE TRAVERSAL OUT PIPE
   test("out: loop", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().out().run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual(['n-1']);
   });
   test("out: label filter passes", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().out('AA').run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual(['n-1']);
   });
   test("out: label filter fails", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().out('BB').run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual([]);
   });
   test("out: object filter passes", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().out({type: 'Sibling'}).run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual(['n-1']);
   });
   test("out: object filter fails", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().out({type: 'Parent'}).run();
     result = result.map(d => d.vertex._id);
 
@@ -181,30 +219,40 @@ describe("cyclic single node graph", () => {
   });
   // SIMPLE TRAVERSAL IN PIPE
   test("in: loop", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().in().run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual(['n-1']);
   });
   test("in: label filter passes", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().out('AA').run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual(['n-1']);
   });
   test("in: label filter fails", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().out('BB').run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual([]);
   });
   test("in: object filter passes", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().in({type: 'Sibling'}).run();
     result = result.map(d => d.vertex._id);
 
     expect(result).toEqual(['n-1']);
   });
   test("in: object filter fails", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().in({type: 'Parent'}).run();
     result = result.map(d => d.vertex._id);
 
@@ -212,6 +260,8 @@ describe("cyclic single node graph", () => {
   });
   // PROPERTY PIPE
   test("property: get colors as result", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     let result = G.v().property('color').run();
     result = result.map(d => d.result);
 
@@ -219,6 +269,8 @@ describe("cyclic single node graph", () => {
   });
   // FAUX PIPE
   test("throws on unrecognized pipetype", () => {
+    // TODO: Resolve type errors on interface
+    // @ts-expect-error: Ignore interface type errors
     expect(() => G.v().thisPipeDoesNotExist().run()).toThrow();
   });
 });
