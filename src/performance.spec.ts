4691c29de8659f55ba7e845b6128a244909577e8
Adamo Carolli <adamo.carolli@gmail.com>
2021-06-29 13:10:16 -0400

    [TESTS] Add basic performance tests for traversals

---
+++ src/performance.spec.ts
@@ -0,0 +1,53 @@
+import { performance } from 'perf_hooks';
+import { bgraph, IGraph } from "./index";
+
+function buildCyclicGraph(n: number) {
+  const nodes = new Array(n).fill(undefined).map((_, i) => {
+    return {_id: i};
+  });
+  const edges = new Array(n).fill(undefined).map((_, i) => {
+    return {
+      _id: i,
+      _out: i,
+      _in: i,
+    };
+  });
+  return [nodes, edges];
+}
+
+describe('traversal performance tests', () => {
+  let G: IGraph;
+  let t0: number;
+  let t1: number;
+  describe.each([
+    {n: 100,   expected: 3},
+    {n: 1000,  expected: 30},
+    {n: 10000, expected: 3000},
+  ])('traversing $n times from single node cyclic graph', ({n, expected}) => {
+    test(`should finish in ${expected}ms`, () => {
+      G = bgraph.graph(...buildCyclicGraph(1));
+      t0 = performance.now();
+      // TODO: Resolve type errors on interface
+      // @ts-expect-error: Ignore interface type errors
+      G.v().start().out().repeat(n).run();
+      t1 = performance.now();
+      expect(t1 - t0).toBeLessThanOrEqual(expected);
+    });
+  });
+
+  describe.each([
+    {n: 10000, expected: 30},
+    {n: 100000, expected: 300},
+    {n: 1000000, expected: 3000},
+  ])('traversing a single time from an $n node cyclic graph', ({n, expected}) => {
+    test(`should finish in ${expected}ms`, () => {
+      G = bgraph.graph(...buildCyclicGraph(n));
+      t0 = performance.now();
+      // TODO: Resolve type errors on interface
+      // @ts-expect-error: Ignore interface type errors
+      G.v().out().run();
+      t1 = performance.now();
+      expect(t1 - t0).toBeLessThanOrEqual(expected);
+    });
+  });
+});
